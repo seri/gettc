b33ba4194b33bc7edeab595865391e3eeab3ebe2
seri <seritrinh@gmail.com>
2014-02-20 22:45:48 +0700

    Add support for proxy

--- core/lib/topcoder/download.rb
+++ core/lib/topcoder/download.rb
@@ -1,6 +1,7 @@
 require 'net/https'
 require 'cgi'
 require 'uri' 
+require 'ostruct'
 
 module TopCoder 
     class Account
@@ -35,7 +36,37 @@ module TopCoder
         LIMIT = 10
         def initialize account
             @account = account
+            @proxy = get_proxy
             @raw = get_cookie
+        end
+        def get_proxy
+            uri = URI.parse ENV['http_proxy']
+            proxy = OpenStruct.new
+            proxy.host, proxy.port = uri.host, uri.port
+            if uri.userinfo then
+                proxy.user, proxy.pass = uri.userinfo.split /:/
+            else
+                proxy.user, proxy.pass = nil, nil
+            end
+            return proxy
+        rescue URI::InvalidURIError
+            return nil
+        end
+        def connect uri
+            if @proxy.nil? then
+                Net::HTTP.start(uri.host, uri.port) do |http|
+                    yield http
+                end
+            else
+                Net::HTTP.start(uri.host, uri.port, @proxy.host, @proxy.port,
+                                @proxy.user, @proxy.pass) do |http|
+                begin
+                    yield http
+                rescue Errno::ECONNRESET
+                    raise DownloadError.new "Check the http_proxy environmental variable"
+                end
+                end
+            end 
         end
         def get_cookie
             uri = URI.join(ROOT, 'tc?&module=Login')
@@ -45,8 +76,9 @@ module TopCoder
                                'password' => @account.password,
                                'rem' => 'on' })
 
-            http = Net::HTTP.new uri.host, uri.port
-            res = http.request req
+            res = connect uri do |http|
+                http.request req
+            end
             raw = res['set-cookie']
 
             cookie = CGI::Cookie.parse raw
@@ -61,20 +93,22 @@ module TopCoder
             unless uri.is_a? URI then
                 uri = url.start_with?('http') ? URI.parse(url) : URI.join(ROOT, url) 
             end 
-            LIMIT.times do
-                req = Net::HTTP::Get.new uri.request_uri
-                req['cookie'] = @raw
 
-                http = Net::HTTP.new uri.host, uri.port
-                res = http.request req   
-                
-                return res.body if res.is_a? Net::HTTPSuccess
-                unless res.is_a? Net::HTTPMovedPermanently then
-                    raise DownloadError.new res.class.to_s 
+            connect uri do |http|
+                LIMIT.times do
+                    req = Net::HTTP::Get.new uri.request_uri
+                    req['cookie'] = @raw
+                    res = http.request req   
+                    
+                    return res.body if res.is_a? Net::HTTPSuccess
+                    unless res.is_a? Net::HTTPMovedPermanently then
+                        raise DownloadError.new res.class.to_s 
+                    end
+                    uri = URI.parse res['location']
                 end
-                uri = URI.parse res['location']
-            end
-            raise DownloadError.new "Tried #{LIMIT} times without success"
+
+                raise DownloadError.new "Tried #{LIMIT} times without success"
+            end 
         end
         def download_problem id
             url = "/stat?c=problem_statement&pm=#{id}"
