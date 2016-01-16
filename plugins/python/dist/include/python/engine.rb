317717d96166f372ca19721a7ae3268a661c9727
seri <seritrinh@gmail.com>
2016-01-16 23:50:28 +0700

    Python plugin works fine

--- plugins/python/dist/include/python/engine.rb
+++ plugins/python/dist/include/python/engine.rb
@@ -1,65 +1,56 @@
 require "gettc/types"
 
 module Gettc
-    class Type
-        def dumb_python
-            if is_a? TArray then
-                return "[]"
-            end
+  class Type
+    def dumb_python
+      return "[]" if self.is_a?(TArray)
 
-            case self
-            when TInt, TLong, TFloat, TDouble
-                return "0"
-            when TChar
-                return "'$'"
-            when TString
-                return '"$"'
-            when TBoolean
-                return "True"
-            end
-
-            return "None"
-        end
+      case self
+      when TInt, TLong, TFloat, TDouble
+        "0"
+      when TChar
+        "'$'"
+      when TString
+        '"$"'
+      when TBoolean
+        "True"
+      else
+        "None"
+      end
     end
-    class PythonEngine
-        attr_reader :arglist, :input, :shebang
-        def initialize func, vars
-            temp = vars.map do |var|
-                var.name
-            end
-            @arglist = temp.join ", "
+  end
 
-            temp = vars.map do |var| 
-                var.name + ' = reader.next("' + var.type.to_s + '")'
-            end
-            @input = temp.join "\nreader.next()\n"
+  class PythonEngine
+    attr_reader :arglist, :input, :shebang
 
-            @shebang = "#! "
-            @shebang << python3_path
-        end
+    def initialize func, vars
+      @arglist = vars.map(&:name).join(", ")
+
+      @input = vars.map do |var|
+        "#{var.name} = reader.next(\"#{var.type}\")"
+      end.join("\nreader.next()\n")
+
+      @shebang = "#! #{python3_path}"
+    end
+
     private
-        def python3_path
-            default = "/usr/bin/env python"
 
-            version = system_q("python --version").split
-            if version.size != 2 || version[0].upcase != "PYTHON" then
-                return default
-            end
+    def python3_path
+      default = "/usr/bin/env python"
 
-            version = version[1].split(".")[0].to_i
-            if version >= 3 then
-                return default
-            end 
+      version = system_q("python --version").split
+      return default if version.size != 2 || version[0].upcase != "PYTHON"
 
-            python3 = system_q "which python3"
-            return python3.empty? ? default : python3
-        rescue
-            return default
-        end
-        def system_q command
-            IO.popen command, err: [:child, :out] do |f|
-                return f.gets
-            end
-        end
+      version = version[1].split(".")[0].to_i
+      return default if version >= 3
+
+      system_q("which python3") || default
+    rescue
+      return default
     end
+
+    def system_q command
+      IO.popen(command, err: [:child, :out]) { |f| f.gets }
+    end
+  end
 end
--- plugins/python/engine_test.rb
+++ plugins/python/engine_test.rb
@@ -1,30 +1,34 @@
 require "test/unit"
-require "gettc/signature" 
-require "engine" 
+require "gettc/signature"
+require "engine"
 include Gettc
 
 class PythonEngineTest < Test::Unit::TestCase
-    def setup
-        func = Signature.new TArray.new(TDouble), "getMaxMin"
-        vars = [ Signature.new(TArray.new(TArray.new(TInt)), "numbers"),
-                 Signature.new(TString, "name"),
-                 Signature.new(TInt, "pivot"),
-                 Signature.new(TBoolean, "rounded") ]
-        @engine = PythonEngine.new func, vars
-    end
-    def test_arglist
-        assert_equal "numbers, name, pivot, rounded", @engine.arglist
-    end
-    def test_input
-        result = <<-eos.gsub(/ {12}/, "")
-            numbers = reader.next("int[][]")
-            reader.next()
-            name = reader.next("String")
-            reader.next()
-            pivot = reader.next("int")
-            reader.next()
-            rounded = reader.next("boolean")
-        eos
-        assert_equal result.rstrip, @engine.input
-    end
+  def setup
+    func = Signature.new(TArray.new(TDouble), "getMaxMin")
+    vars = [
+      Signature.new(TArray.new(TArray.new(TInt)), "numbers"),
+      Signature.new(TString, "name"),
+      Signature.new(TInt, "pivot"),
+      Signature.new(TBoolean, "rounded")
+    ]
+    @engine = PythonEngine.new func, vars
+  end
+
+  def test_arglist
+    assert_equal "numbers, name, pivot, rounded", @engine.arglist
+  end
+
+  def test_input
+    result = <<-eos.gsub(/ {6}/, "")
+      numbers = reader.next("int[][]")
+      reader.next()
+      name = reader.next("String")
+      reader.next()
+      pivot = reader.next("int")
+      reader.next()
+      rounded = reader.next("boolean")
+    eos
+    assert_equal result.rstrip, @engine.input
+  end
 end
